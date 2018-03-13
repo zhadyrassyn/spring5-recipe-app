e3ce50b0d6e7d9c0e20ff939719d6fe188ed5aba
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-03-13 18:02:06 +0600

    Database initialization.

--- src/main/java/guru/springframework/repositories/RecipeRepository.java
+++ src/main/java/guru/springframework/repositories/RecipeRepository.java
@@ -2,6 +2,7 @@ package guru.springframework.repositories;
 
 import guru.springframework.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by jt on 6/13/17.
