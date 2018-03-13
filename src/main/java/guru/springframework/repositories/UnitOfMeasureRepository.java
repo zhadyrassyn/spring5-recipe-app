28d803b3fdafe489cc51cbf9d575d36547faddb3
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-03-13 12:50:33 +0600

    Spring data. Models, Repos

---
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasureRepository, Long>{
+}
